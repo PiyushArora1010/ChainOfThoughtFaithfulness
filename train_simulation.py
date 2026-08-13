1b0ec424ff38a302324ce94f6b321d8e5444d35b
Piyush Arora <piyusharora@Piyushs-MacBook-Pro.local>
2026-08-13 22:16:57 +0100

    remove eval mentions

--- train_simulation.py
+++ train_simulation.py
@@ -253,13 +253,9 @@ if __name__ == '__main__':
         # Batching
         per_device_train_batch_size=args.model_batch_size,
         gradient_accumulation_steps=args.gradient_accumulation_steps,
-        per_device_eval_batch_size=args.model_batch_size,
-        eval_accumulation_steps=args.gradient_accumulation_steps,
 
         # Logging / checkpointing
         logging_steps=args.logging_steps,
-        eval_strategy="steps",
-        eval_steps=args.eval_steps,
         save_steps=args.save_steps,
         report_to="wandb" if (args.wandb and engine.local_rank == 0) else "none",
         run_name=RUN_NAME,
